32c0cce36b6d755d586593343966b9ac1e159024
Fernando Ortiz <fernandodev.ortiz@gmail.com>
2022-02-03 12:20:07 -0600

    include lang attribute to Html component on _document.js file

--- next.config.js
+++ next.config.js
@@ -14,3 +14,9 @@ module.exports = {
     return config 
   },
 };
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
+
+module.exports = withBundleAnalyzer({});
